4a0ffd80b26120af555428e091c9a9742b3f010a
Josh Johnson <SnoFox@SnoFox.net>
2010-11-19 13:37:52 -0800

    Made it more better ;)

--- foxbot.php
+++ foxbot.php
@@ -2,17 +2,15 @@
 /* Example bot usage of the FoxBot PHP IRC Bot framework */
 
 include 'foxbot/foxbot.php';
-logit( 'Included the bot!' );
+
 $nick = 'FawkzBawt';
 $ident = 'Fawkz';
 $bind = '192.168.1.100';
 $gecos = 'FoxBot IRC Bot';
 
-$ClueNet = Bot::start( $nick, $ident, $gecos, 'irc.cluenet.org', 6667, 0, $bind );
-logit( 'ClueNet started!' );
-$SleepyIRC = Bot::start( $nick, $ident, $gecos, 'irc.sleepyirc.net', 6667, 0, $bind );
-logit( 'SleepyIRC started!' );
-$bots = array( $ClueNet, $SleepyIRC );
-Bot::mainLoop($bots);
+$ClueNet = new Bot( $nick, $ident, $gecos, 'irc.cluenet.org', 6667, FALSE, $bind );
+$SleepyIRC = new Bot( $nick, $ident, $gecos, 'irc.sleepyirc.net', 6667, FALSE, $bind );
+
+Bot::mainLoop();
 
 die( 'Broke out of an infinite loop! My life is complete... Bye bye!' . "\n" );
--- foxbot/classes/Bot.php
+++ foxbot/classes/Bot.php
@@ -1,63 +1,92 @@
 <?php
 
 class Bot {
-    protected $socket; // Socket this session is attached to
+    
+    private static $botHandlers = array(); // Array of bot objects
 
     // IRC Session stuff
+    protected $socket; // Socket this session is attached to
+    protected $connected; // Connected to IRC or no
     protected $nick; // Bot's current nick
     protected $serverHost; // DNS hostname
     protected $serverName; // What the server calls itself
+    protected $serverPass; // Accounting for the /PASS command...
     protected $port; // The port we used
     protected $ssl; // Using SSL or no
     protected $vHost; // Host we bound to
     protected $host; // Bot's host on IRC (maybe!)
     protected $ident; // Bots identd on IRC (maybe!)
 
-    public static function start( $nick, $ident, $gecos, $server, $port = 6667, $ssl = 0, $host = NULL ) {
-        if( $ssl == 0 ) {
+    function __construct( $nick, $ident, $gecos, $server, $port = 6667, $ssl = FALSE, $host = NULL, $pass = NULL ) {
+        $this->nick = $nick;
+        $this->ident = $ident;
+        $this->gecos = $gecos;
+        $this->serverHost = $server;
+        $this->port = $port;
+        $this->serverPass = $pass;
+        $this->ssl = $ssl;
+        $this->vHost = $host;
+        $this->connected = FALSE;
+
+        // Can't set these until we get on IRC
+        $this->serverName = NULL;
+        $this->host = NULL;
+
+        self::$botHandlers[] = $this;
+    }
+
+    function __destruct() {
+        // Quit the bot from IRC if it's on it
+        if( !feof( $this->socket ) ) {
+            IRC::raw( $this->socket, 'QUIT :Being sent to destruction...' );
+        }
+        // Remove the bot from the list of bots
+        foreach( self::$botHandlers as $handlerKey => $botHandler ) {
+            if( $botHandler === $botHandlers[$handlerKey] ) {
+                unset( $botHandlers[$handlerKey] );
+            }
+        }
+    }
+
+    function init() {
+
+        if( $this->ssl === FALSE ) {
             $transport = 'tcp://';
         } else {
             $transport = 'ssl://';
         }
-        $uri = $transport . $server . ':' . $port;
+        $uri = $transport . $this->serverHost . ':' . $this->port;
 
         logit( 'Connecting to ' . $uri . '...', 'notice' );
 
-        $socket = stream_socket_client( $uri,
+        $this->socket = stream_socket_client( $uri,
             $errNo,
             $errStr,
             30,
             STREAM_CLIENT_CONNECT,
-            stream_context_create( array( 'socket' => array( 'bindto' => $host.':0' ) ) )
+            stream_context_create( array( 'socket' => array( 'bindto' => $this->vHost.':0' ) ) )
         );
-        if( !$socket )
+        if( !$this->socket )
             throw new Exception( 'Failed to connect to IRC: ' . $errStr . '(' . $errNo . ')' . "\n" );
-
-        return new self( $socket, $server, $port, $ssl, $host );
+var_dump($this->ident);
+        if( $this->serverPass !== NULL )
+            IRC::raw( $this->socket, 'PASS :' . $this->pass );
+        //IRC::raw( $this->socket, 'USER ' . $this->ident . ' ' . $this->vHost . ' ' . $this->serverHost . ' :' . $this->gecos ); 
+        IRC::raw( $this->socket, 'USER ' . $this->ident . ' ' . ($this->vHost === NULL ? 0 : $this->vHost) . ' ' . $this->serverHost . ' :' . $this->gecos );
+        // IRC::raw( $this->socket, 'USER foxbot 1 1 :gecos here');
+        IRC::raw( $this->socket, 'NICK ' . $this->nick );
     }
 
-    protected function __construct( $socket, $server, $port, $ssl, $host ) {
-        $this->socket = $socket;
-        $this->serverHost = $server;
-        $this->port = $port;
-        $this->ssl = $ssl;
-        $this->host = $host;
-    }
-
-    function __destruct() {
-        if( !feof( $this->socket ) ) {
-            IRC::raw( $this->socket, 'QUIT :Being sent to destruction...' );
-        }
-    }
-
-
-    public static function mainLoop( $botHandlers ) {
+    public static function mainLoop() {
         logit( 'Started main loop...', 'debug' );
+        foreach( self::$botHandlers as $botHandler ) {
+            $botHandler->init();
+        }
         while( TRUE ) {
 
             $sockets = array();
 
-            foreach( $botHandlers as $currHandler ) {
+            foreach( self::$botHandlers as $currHandler ) {
                 $sockets[] = $currHandler->socket;
             }
             $null = NULL; // because I can :D
@@ -72,14 +101,14 @@ class Bot {
 
                 $currentBot = NULL;
 
-                foreach( $botHandlers as $handlerKey => $currHandler ) {
+                foreach( self::$botHandlers as $handlerKey => $currHandler ) {
                     if( $currHandler->socket === $currSocket ) {
                         $currentBot = $handlerKey;
                     } // If this socket = current socket
                 } // foreach bothandler...
                 if( $currentBot === NULL )
                     throw new Exception( 'Read from a socket I don\'t know about... Wtf?' );
-                $currentBot = $botHandlers[$currentBot];
+                $currentBot = self::$botHandlers[$currentBot];
 
                 if( !feof( $currentBot->socket ) ) {
                     $ircData = fgets( $currentBot->socket, 512 );
